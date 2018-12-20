28aebf10197cda5e19007b545914cfe3672bccf5
zhengjinpeng <709691139@qq.com>
2018-12-20 22:00:18 +0800

    1

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
 // index.js
 
-'use strict';
+"use strict";
 
-import Theme from './themes/Theme';
-import Label from './components/Label/Label';
-import Button from './components/Button/Button';
-import Checkbox from './components/Checkbox/Checkbox';
-import Input from './components/Input/Input';
-import Select from './components/Select/Select';
-import Stepper from './components/Stepper/Stepper';
-import SearchInput from './components/SearchInput/SearchInput';
-import Badge from './components/Badge/Badge';
-import Popover from './components/Popover/Popover';
+import Theme from "./themes/Theme";
+import Label from "./components/Label/Label";
+import Button from "./components/Button/Button";
+import Checkbox from "./components/Checkbox/Checkbox";
+import Input from "./components/Input/Input";
+import Select from "./components/Select/Select";
+import Stepper from "./components/Stepper/Stepper";
+import SearchInput from "./components/SearchInput/SearchInput";
+import Badge from "./components/Badge/Badge";
+import Popover from "./components/Popover/Popover";
 
-import NavigationBar from './components/NavigationBar/NavigationBar';
-import ListRow from './components/ListRow/ListRow';
-import Carousel from './components/Carousel/Carousel';
-import Projector from './components/Projector/Projector';
-import SegmentedBar from './components/SegmentedBar/SegmentedBar';
-import SegmentedView from './components/SegmentedView/SegmentedView';
-import TabView from './components/TabView/TabView';
-import TransformView from './components/TransformView/TransformView';
-import AlbumView from './components/AlbumView/AlbumView';
-import Wheel from './components/Wheel/Wheel';
+import NavigationBar from "./components/NavigationBar/NavigationBar";
+import ListRow from "./components/ListRow/ListRow";
+import Carousel from "./components/Carousel/Carousel";
+import Projector from "./components/Projector/Projector";
+import SegmentedBar from "./components/SegmentedBar/SegmentedBar";
+import SegmentedView from "./components/SegmentedView/SegmentedView";
+import TabView from "./components/TabView/TabView";
+import TransformView from "./components/TransformView/TransformView";
+import AlbumView from "./components/AlbumView/AlbumView";
+import Wheel from "./components/Wheel/Wheel";
 
-import Overlay from './components/Overlay/Overlay';
-import Toast from './components/Toast/Toast';
-import ActionSheet from './components/ActionSheet/ActionSheet';
-import ActionPopover from './components/ActionPopover/ActionPopover';
-import PullPicker from './components/PullPicker/PullPicker';
-import PopoverPicker from './components/PopoverPicker/PopoverPicker';
-import Menu from './components/Menu/Menu';
-import Drawer from './components/Drawer/Drawer';
-import ModalIndicator from './components/ModalIndicator/ModalIndicator';
+import Overlay from "./components/Overlay/Overlay";
+import Toast from "./components/Toast/Toast";
+import ActionSheet from "./components/ActionSheet/ActionSheet";
+import ActionPopover from "./components/ActionPopover/ActionPopover";
+import PullPicker from "./components/PullPicker/PullPicker";
+import PopoverPicker from "./components/PopoverPicker/PopoverPicker";
+import Menu from "./components/Menu/Menu";
+import Drawer from "./components/Drawer/Drawer";
+import ModalIndicator from "./components/ModalIndicator/ModalIndicator";
 
-import TeaNavigator from './components/TeaNavigator/TeaNavigator';
-import BasePage from './components/BasePage/BasePage';
-import NavigationPage from './components/NavigationPage/NavigationPage';
+import TeaNavigator from "./components/TeaNavigator/TeaNavigator";
+import BasePage from "./components/BasePage/BasePage";
+import NavigationPage from "./components/NavigationPage/NavigationPage";
 
-import KeyboardSpace from './components/KeyboardSpace/KeyboardSpace';
+import KeyboardSpace from "./components/KeyboardSpace/KeyboardSpace";
 
 var Teaset = {
   Theme,
@@ -78,7 +78,7 @@ var Teaset = {
   BasePage,
   NavigationPage,
 
-  KeyboardSpace,
+  KeyboardSpace
 };
 
-module.exports = Teaset;
+export default Teaset;
